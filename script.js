5878e82a1444c6f168573185ca0ba04a17a47219
Eton Miller <etonmiller5@gmail.com>
2026-03-25 02:22:40 -0400

    Created two methods (getTotalValue and toString)

--- script.js
+++ script.js
@@ -4,4 +4,12 @@ class ProductProperties {
         this.price = price;
         this.quantity = quantity;
     }
+
+    getTotalValue() {
+        return this.price * this.quantity;
+    }
+
+    toString() {
+        return `Product: ${this.name}, Price: ${this.price}, Quantity: ${this.quantity}}`;
+    }
 }
