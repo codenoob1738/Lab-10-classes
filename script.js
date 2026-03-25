343c43fd1ded91f0d0a723a22e69fdd9a50fdf93
Eton Miller <etonmiller5@gmail.com>
2026-03-25 02:45:49 -0400

    Created two instances of the SubClass created. Also updated the toString to remove a curly bracket from console log

--- script.js
+++ script.js
@@ -21,6 +21,14 @@ class PerishableProductProperties extends ProductProperties {
     }
 
     toString() {
-        return `${super.toString()}, Expiration Date: ${this.expirationDate}`;  //Overrides the toString method from the parent class to include the expiration date.//
+        return `Product: ${this.name}, Price: ${this.price}, Quantity: ${this.quantity}, Expiration Date: ${this.expirationDate}`;  //Overrides the toString method from the parent class to include the expiration date.//
     }
 }
+
+// Instances of the PerishableProductProperties class//
+
+const item1 = new PerishableProductProperties("Lettuce", 2.59, 1, "2026-03-31");
+const item2 = new PerishableProductProperties("Ice Cream", 4.99, 1, "2027-04-15");
+
+console.log(item1.toString());
+console.log(item2.toString());
